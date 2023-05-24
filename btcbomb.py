eb26a980e913be1938c36022a2e070cb745f05a4
OhirLex <95553621+OhirLex@users.noreply.github.com>
2023-05-24 05:48:01 +0300

    Update btcbomb.py

--- btcbomb.py
+++ btcbomb.py
@@ -76,6 +76,78 @@ while 1:
                                 tel_no6 = number.strip()
                             elif i == 6:
                                 tel_no7 = number.strip()
+                            elif i == 7:
+                                tel_no8 = number.strip()
+                            elif i == 8:
+                                tel_no9 = number.strip()
+                            elif i == 9:
+                                tel_no10 = number.strip()
+                            elif i == 10:
+                                tel_no11 = number.strip()
+                            elif i == 11:
+                                tel_no12 = number.strip()
+                            elif i == 12:
+                                tel_no13 = number.strip()
+                            elif i == 13:
+                                tel_no14 = number.strip()
+                            elif i == 14:
+                                tel_no15 = number.strip()
+                            elif i == 15:
+                                tel_no16 = number.strip()
+                            elif i == 16:
+                                tel_no17 = number.strip()
+                            elif i == 17:
+                                tel_no18 = number.strip()
+                            elif i == 18:
+                                tel_no19 = number.strip()
+                            elif i ==19:
+                                tel_no20 = number.strip()
+                            elif i == 20:
+                                tel_no21 = number.strip()
+                            elif i == 21:
+                                tel_no22 = number.strip()
+                            elif i == 22:
+                                tel_no23 = number.strip()
+                            elif i == 23:
+                                tel_no24 = number.strip()
+                            elif i == 24:
+                                tel_no25 = number.strip()
+                            elif i == 25:
+                                tel_no26 = number.strip()
+                            elif i == 26:
+                                tel_no27 = number.strip()
+                            elif i == 27:
+                                tel_no28 = number.strip()
+                            elif i == 28:
+                                tel_no29 = number.strip()
+                            elif i == 29:
+                                tel_no30 = number.strip()
+                            elif i == 30:
+                                tel_no31 = number.strip()
+                            elif i == 31:
+                                tel_no32 = number.strip()
+                            elif i == 32:
+                                tel_no33 = number.strip()
+                            elif i == 33:
+                                tel_no34 = number.strip()
+                            elif i == 34:
+                                tel_no35 = number.strip()
+                            elif i == 35:
+                                tel_no36 = number.strip()
+                            elif i == 36:
+                                tel_no37 = number.strip()
+                            elif i == 37:
+                                tel_no38 = number.strip()
+                            elif i == 38:
+                                tel_no39 = number.strip()
+                            elif i == 39:
+                                tel_no40 = number.strip()
+                            elif i == 40:
+                                tel_no41 = number.strip()
+                            elif i == 41:
+                                tel_no42 = number.strip()
+                            elif i == 42:
+                                tel_no43 = number.strip()
                             if len(number.strip()) != 10:
                                 raise ValueError
                         if i<6:
@@ -92,6 +164,78 @@ while 1:
                                     tel_no6 = "bos"
                                 elif j==6:
                                     tel_no7 = "bos"
+                                elif j==7:
+                                    tel_no8 = "bos"
+                                elif j==8:
+                                    tel_no9 = "bos"
+                                elif j==9:
+                                    tel_no10 = "bos"
+                                elif j==10:
+                                    tel_no11 = "bos"
+                                elif j==11:
+                                    tel_no12 = "bos"
+                                elif j==12:
+                                    tel_no13 = "bos"
+                                elif j==13:
+                                    tel_no14 = "bos"
+                                elif j==14:
+                                    tel_no15 = "bos"
+                                elif j==15:
+                                    tel_no16 = "bos"
+                                elif j==16:
+                                    tel_no17 = "bos"
+                                elif j==17:
+                                    tel_no18 = "bos"
+                                elif j==18:
+                                    tel_no19 = "bos"
+                                elif j==19:
+                                    tel_no20 = "bos"
+                                elif j==20:
+                                    tel_no21 = "bos"
+                                elif j==21:
+                                    tel_no22 = "bos"
+                                elif j==22:
+                                    tel_no23 = "bos"
+                                elif j==23:
+                                    tel_no24 = "bos"
+                                elif j==24:
+                                    tel_no25 = "bos"
+                                elif j==25:
+                                    tel_no26 = "bos"
+                                elif j==26:
+                                    tel_no27 = "bos"
+                                elif j==27:
+                                    tel_no28 = "bos"
+                                elif j==28:
+                                    tel_no29 = "bos"
+                                elif j==29:
+                                    tel_no30 = "bos"
+                                elif j==30:
+                                    tel_no31 = "bos"
+                                elif j==31:
+                                    tel_no32 = "bos"
+                                elif j==32:
+                                    tel_no33 = "bos"
+                                elif j==33:
+                                    tel_no34 = "bos"
+                                elif j==34:
+                                    tel_no35 = "bos"
+                                elif j==35:
+                                    tel_no36 = "bos"
+                                elif j==36:
+                                    tel_no37 = "bos"
+                                elif j==37:
+                                    tel_no38 = "bos"
+                                elif j==38:
+                                    tel_no39 = "bos"
+                                elif j==39:
+                                    tel_no40 = "bos"
+                                elif j==40:
+                                    tel_no41 = "bos"
+                                elif j==41:
+                                    tel_no42 = "bos"
+                                elif j==42:
+                                    tel_no43 = "bos"
                 except FileNotFoundError:
                     system("cls||clear")
                     print(Fore.LIGHTRED_EX + "Dosya bulunamadı. Tekrar deneyiniz.")
